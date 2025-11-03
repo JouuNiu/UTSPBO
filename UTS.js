f08fd494367a3304b0d8194d2f5aac6357c1cfd9
JouuNiu <ini.yoga30@gmail.com>
2025-11-03 09:40:45 +0800

    utslanjutan

--- UTS.js
+++ UTS.js
@@ -6,6 +6,35 @@ class kuotaCuti{
         this.cutiMelahirkan = 90;
     }
 }
-getKuotaCuti(){
-    if
+class cutiTahunan extends kuotaCuti{
+    getCuti(){
+    if(this.cutiTahunan > 12){
+        return `Cuti Tahunan anda sudah melebihi batas maksimal`;
+    } else {
+        return `Sisa cuti tahunan anda adalah ${this.cutiTahunan} hari`;
+    }
 }
+}
+
+class cutiSakit extends kuotaCuti{
+    getCutiSakit(){
+    if(this.cutiSakit > 2){
+        return `Cuti Sakit anda sudah melebihi batas maksimal`;
+    } else {
+        return `Sisa cuti sakit anda adalah ${this.cutiSakit} hari`;
+        }
+    }
+}
+
+
+class cutiMelahirkan extends kuotaCuti{
+    getCutiMelahirkan(){
+    if(this.cutiMelahirkan > 90){
+        return `Cuti Melahirkan anda sudah melebihi batas maksimal`;  
+    } else {
+        return `Sisa cuti melahirkan anda adalah ${this.cutiMelahirkan} hari`;
+    }
+}
+}
+
+const karyawan1 = new kuotaCuti('Alya', 10, 1, 80);
