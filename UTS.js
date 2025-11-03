780ca9db5a04d78f77720a08a6b84958341032ea
JouuNiu <ini.yoga30@gmail.com>
2025-11-03 09:28:15 +0800

    UTS

---
+++ UTS.js
@@ -0,0 +1,11 @@
+class kuotaCuti{
+    constructor(nama, cutiTahunan, cutiSakit, cutiMelahirkan ){
+        this.nama = nama;
+        this.cutiTahunan = 12;
+        this.cutiSakit = 2;
+        this.cutiMelahirkan = 90;
+    }
+}
+getKuotaCuti(){
+    if
+}
